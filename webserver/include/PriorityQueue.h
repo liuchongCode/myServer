91c56bb4ccb687fc7f793c2bea4fc0070c0461d5
liuchongCode <110719219+liuchongCode@users.noreply.github.com>
2022-08-28 21:54:37 +0800

    Update PriorityQueue.h
    删除hash表，自身增加锁机制

--- webserver/include/PriorityQueue.h
+++ webserver/include/PriorityQueue.h
@@ -5,37 +5,45 @@
 #include <vector>
 #include "http_connect.h"
 #include <unordered_map>
+#include "Log/Logger.h"
 
 
 struct client
 {
     bool deleted_;
+    int index_;
     int fd_;
     std::shared_ptr<http_connect> user_;
     size_t expiredTime_;
-    client() : deleted_(false), expiredTime_(-1) {}
+    client() : index_(-1), deleted_(false), expiredTime_(-1) {}
 };
 
 class PriorityQueue
 {
 private:
-    std::unordered_map<int, size_t> map_;   // fd映射到heap_中的的索引
+    // std::unordered_map<int, size_t> map_;   // fd映射到heap_中的的索引
     std::vector<client> heap_;
     int size_;
+    std::mutex mutex_;
 public:
     PriorityQueue() : size_(0) {}
     ~PriorityQueue() {
     }
-
-    void add(std::shared_ptr<http_connect>, int timeout);
-    void update(int index, int val);
-    int top();
+    bool isContain(const std::shared_ptr<http_connect>& user) {
+        if (!user)
+            return false;
+        std::unique_lock<std::mutex> lock(mutex_);
+        return user->timer != nullptr;
+    }
+    size_t top();
     void pop();
     int UpToDown(int i, int len);
     int DownToUp(int len);
     void swap(int x, int y);
+    void add(std::shared_ptr<http_connect>& user, int timeout);
+    void update(const std::shared_ptr<http_connect>& user, int val);
+    bool setDeleted(const std::shared_ptr<http_connect>& user);
     void handleExpiredClient();
-    bool setDeleted(int fd);
     int getnexttick();
     void show() {
         size_t tmp = heap_[0].expiredTime_;
